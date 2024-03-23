b711510eb1ddcd780affc89cc13c15bf83beb38d
Ian Galaviz <ig25049@judgememorial.com>
2024-03-23 13:07:23 -0600

    Shoutout 3245 for the Spark Flex :))))

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -3,26 +3,34 @@ package frc.robot.subsystems;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IntakeConstants;
 
 public class IntakeSubsystem extends SubsystemBase {
     private final TalonFX rightIntakeMotor;
     private final TalonFX leftIntakeMotor;
-    private final TalonFX indexMotor;
+    // private final TalonFX indexMotor;
+
+    private final CANSparkMax indexMotor;
 
     public IntakeSubsystem() {
         rightIntakeMotor = new TalonFX(IntakeConstants.rightIntakeID);
         leftIntakeMotor = new TalonFX(IntakeConstants.leftIntakeID);
-        indexMotor = new TalonFX(IntakeConstants.indexID);
+        indexMotor = new CANSparkMax(IntakeConstants.indexID, MotorType.kBrushless);
 
         rightIntakeMotor.setNeutralMode(NeutralModeValue.Coast);
         leftIntakeMotor.setNeutralMode(NeutralModeValue.Coast);
-        indexMotor.setNeutralMode(NeutralModeValue.Coast);
+        indexMotor.setIdleMode(IdleMode.kCoast);
 
         rightIntakeMotor.setInverted(false);
         leftIntakeMotor.setInverted(false);
         indexMotor.setInverted(true);
+
+        indexMotor.burnFlash();
     }
 
     @Override
--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -4,23 +4,36 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
+import com.revrobotics.CANSparkFlex;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
 import frc.robot.Constants.ShooterConstants;
 
 public class ShooterSubsystem extends SubsystemBase {
-    private final TalonFX rightShooterMotor;
+    // private final TalonFX rightShooterMotor;
+    private final CANSparkFlex rightShooterMotor;
     private final TalonFX leftShooterMotor;
 
     public ShooterSubsystem() {
-        rightShooterMotor = new TalonFX(ShooterConstants.rightShooterID);
-        addChild("rightShooterTalon", rightShooterMotor);
+        // rightShooterMotor = new TalonFX(ShooterConstants.rightShooterID);
+        // addChild("rightShooterTalon", rightShooterMotor);
+        // rightShooterMotor.setInverted(true);
+
+        rightShooterMotor = new CANSparkFlex(ShooterConstants.rightShooterID, MotorType.kBrushless);
         rightShooterMotor.setInverted(true);
 
         leftShooterMotor = new TalonFX(ShooterConstants.leftShooterID);
         addChild("leftShooterTalon", leftShooterMotor);
         leftShooterMotor.setInverted(false);
 
-        rightShooterMotor.setNeutralMode(NeutralModeValue.Brake);
+        // rightShooterMotor.setNeutralMode(NeutralModeValue.Brake);
+        rightShooterMotor.setIdleMode(IdleMode.kCoast);
         leftShooterMotor.setNeutralMode(NeutralModeValue.Brake);
+
+        rightShooterMotor.setOpenLoopRampRate(1);
+
+        rightShooterMotor.burnFlash();
     }
 
     @Override
